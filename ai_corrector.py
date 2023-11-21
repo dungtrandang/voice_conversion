93826af37c3a52c14c651b879468661b4050309f
dungtrandang <135292518+dungtrandang@users.noreply.github.com>
2023-11-21 12:55:19 +0000

    modify the prompt

--- ai_corrector.py
+++ ai_corrector.py
@@ -8,6 +8,7 @@ def correctness(text):
         {"role": "system", "content": "You are a language expert assistant. \
         Your job is to fix the grammar and word usage errors \
         Only respond the fixed text, do not explain or say anything else \
+        Translate the text into English if it's not written in English \
         Shorten your response in 100 characters at most. "},
         {"role": "user", "content": f"{text}"}
     ]
