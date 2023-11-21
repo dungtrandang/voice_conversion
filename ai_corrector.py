509a1a7c55bd9dff2e1d212e2c14f198fb0b649d
dungtrandang <135292518+dungtrandang@users.noreply.github.com>
2023-11-21 11:31:22 +0000

    fix response of chatgpt

--- ai_corrector.py
+++ ai_corrector.py
@@ -5,10 +5,11 @@ def correctness(text):
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a language refinement assistant. \
-        Refine chat for natural flow. \
+        {"role": "system", "content": "You are a language expert assistant. \
+        Your job is to fix the grammar and word usage errors \
+        Only respond the fixed text, do not explain or say anything else \
         Shorten your response in 100 characters at most. "},
-        {"role": "user", "content": f"Refine chat for natural flow: {text}"}
+        {"role": "user", "content": f"{text}"}
     ]
     )
     return completion.choices[0].message.content
